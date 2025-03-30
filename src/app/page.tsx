5977b4f442034a94829cd8203a9cc3e6f598c41b
ayush mhetre <113123869+Volentry@users.noreply.github.com>
2025-03-31 02:36:00 +0530

    check

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div>
+    <h1>hi</h1>
+    <Button>sign in </Button>
+   </div>
+  );
+}
